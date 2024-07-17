383c82dfd59fce3695b8bd9692aa56a4b1e29f14
elvysbatzibal <ebatzibal@plusti.com>
2024-07-16 22:47:26 -0600

    ADD Aplicacion base

---
+++ ClientSK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http.Json;
+
+using HttpClient hc = new() { BaseAddress = new("https://localhost:7168") };
+while (true)
+{
+    Console.Write("Question: ");
+
+    await foreach (var msg in hc.GetFromJsonAsAsyncEnumerable<string>($"/copilot?question={Console.ReadLine()}"))
+        Console.Write(msg);
+
+    Console.WriteLine();
+}
---
+++ CoreSK/Controllers/CopilotController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreSK.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CopilotController : Controller
+    {
+
+
+        // GET api/values/5
+        //[HttpGet("{question}")]
+        [HttpGet]
+        public IAsyncEnumerable<string> Get([FromQuery] string question)
+        {
+            return GetResponseAsync(question);
+        }
+
+        static async IAsyncEnumerable<string> GetResponseAsync(string question)
+        {
+
+            foreach (string word in question.Split(' '))
+
+            {
+
+                await Task.Delay(250);
+
+                yield return word + " ";
+
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string question)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
